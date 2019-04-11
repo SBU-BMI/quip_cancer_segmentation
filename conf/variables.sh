af9c95d2c83d3e935e128778a94b42e3899b6c0d
Han Le <32404085+hansbu@users.noreply.github.com>
2019-04-10 22:16:18 -0400

    Update variables.sh

--- conf/variables.sh
+++ conf/variables.sh
@@ -9,21 +9,22 @@ MONGODB_PORT=27017
 HEATMAP_VERSION=cancer_BC_v2
 
 # Base directory
-BASE_DIR=/data01/shared/hanle/tumor_project/train_tumor_classification/quip_cancer_segmentation
+BASE_DIR=/workspace/
+BASE_OUT=/home/data/
 
 # The list of case_ids you want to download heaetmaps from
 CASE_LIST=${BASE_DIR}/data/raw_marking_to_download_case_list/case_list.txt
-DATA_PATH='/data01/shared/hanle/tumor_project/breast_cancer_40X/cancer_pos1'        # Change this to your training data folder
+DATA_PATH=${BASE_OUT}/training_data        # Change this to your training data folder
 DATA_LIST='tumor_data_list_toy.txt'        # Text file to contain subfolders for testing (1st line), training (the rest)
 MODEL='RESNET_34_cancer_350px_lr_1e-2_decay_5_jitter_val6slides_harder_pretrained_cancer_tils_none_1117_1811_0.9157633018398808_9.t7'     # model is saved in data/models_cnn
 
 
 # Paths of data, log, input, and output
 JSON_OUTPUT_FOLDER=${BASE_DIR}/data/heatmap_jsons
-HEATMAP_TXT_OUTPUT_FOLDER=${BASE_DIR}/data/heatmap_txt
-LOG_OUTPUT_FOLDER=${BASE_DIR}/data/log
-SVS_INPUT_PATH=${BASE_DIR}/data/svs
-PATCH_PATH=${BASE_DIR}/data/patches
+HEATMAP_TXT_OUTPUT_FOLDER=${BASE_OUT}/heatmap_txt
+LOG_OUTPUT_FOLDER=${BASE_OUT}/log
+SVS_INPUT_PATH=${BASE_OUT}/svs
+PATCH_PATH=${BASE_OUT}/patches
 PATCH_SAMPLING_LIST_PATH=${BASE_DIR}/data/patch_sample_list
 RAW_MARKINGS_PATH=${BASE_DIR}/data/raw_marking_xy
 MODIFIED_HEATMAPS_PATH=${BASE_DIR}/data/modified_heatmaps
@@ -35,14 +36,7 @@ THRESHOLDED_HEATMAPS_PATH=${BASE_DIR}/data/thresholded_heatmaps
 PATCH_FROM_HEATMAP_PATH=${BASE_DIR}/data/patches_from_heatmap
 THRESHOLD_LIST=${BASE_DIR}/data/threshold_list/threshold_list.txt
 
-CAE_TRAINING_DATA=${BASE_DIR}/data/training_data_cae
-CAE_TRAINING_DEVICE=gpu0
-CAE_MODEL_PATH=${BASE_DIR}/data/models_cae
-LYM_CNN_TRAINING_DATA=${BASE_DIR}/data/training_data_cnn
-LYM_CNN_TRAINING_DEVICE=gpu0
-LYM_CNN_PRED_DEVICE=gpu0
 LYM_NECRO_CNN_MODEL_PATH=${BASE_DIR}/data/models_cnn
-NEC_CNN_TRAINING_DATA=${BASE_DIR}/data/training_data_cnn
-NEC_CNN_TRAINING_DEVICE=gpu1
-NEC_CNN_PRED_DEVICE=gpu0
-
+LYM_CNN_TRAINING_DATA=${BASE_DIR}/data/training_data_cnn
+LYM_CNN_TRAINING_DEVICE=0
+LYM_CNN_PRED_DEVICE=0

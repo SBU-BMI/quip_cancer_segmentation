7cbb03906f724628a8be0cb4a0d31b1f2d38870a
Han Le <hdle@login1.summit.olcf.ornl.gov>
2020-03-06 11:19:25 -0500

    change setting of jsrun in patch extraction

--- _jobs_summit/0_user_input.sh
+++ _jobs_summit/0_user_input.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # *****User input: change location for svs folder and output folder
-SVS_DIR="/gpfs/alpine/med108/proj-shared/hdle/data_sample_brca/svs"
+SVS_DIR="/gpfs/alpine/med108/proj-shared/hdle/data_sample_brca/svs_large"
 OUT_DIR="/gpfs/alpine/med108/proj-shared/hdle/data_sample_brca_out"
 CODE_DIR="/gpfs/alpine/med108/proj-shared/hdle/quip_cancer_segmentation"
 # end of user input

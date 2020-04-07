ed905edaf1b7cfd0fbec4fef0678acdd63b85931
Han Le <hdle@login4.summit.olcf.ornl.gov>
2020-04-07 13:50:09 -0400

    mv json files to data/

--- _jobs_summit/run_next_jobs.py
+++ _jobs_summit/run_next_jobs.py
@@ -2,5 +2,7 @@ import os
 import time
 
 if __name__ == '__main__':
-    time.sleep(6*60*60)
+    time.sleep(4*60*60)
+    os.system('bash 3_start_prediction.sh 100')
+    time.sleep(4*60*60)
     os.system('bash 3_start_prediction.sh 100')

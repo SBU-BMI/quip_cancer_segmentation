cc893857c378525f938edda81caca25d82911d68
tkurc <tkurc@harrier.cm.cluster>
2022-08-08 10:47:44 -0400

    Fix filename handling.

--- heatmap_gen/cp_heatmaps_all.sh
+++ heatmap_gen/cp_heatmaps_all.sh
@@ -7,7 +7,10 @@ FOLDER=${1}     #/data/patches
 PRED_VERSION=patch-level-cancer.txt
 DIS_FOLDER=${OUT_DIR}/patch-level-lym/
 for files in ${FOLDER}/*/${PRED_VERSION}; do
-    dis=`echo ${files} | awk -F'/' '{print "prediction-"substr($(NF-1),1,length($(NF-1))-4);}'`
+    fname=`echo ${files} | awk -F '/' '{print $(NF-1)}'`;
+    fprefix="${fname%.*}"
+    dis="prediction-"$fprefix
+    # dis=`echo ${files} | awk -F'/' '{print "prediction-"substr($(NF-1),1,length($(NF-1))-4);}'`
     cp ${files} ${DIS_FOLDER}/${dis}
 done
 
@@ -21,7 +24,10 @@ done
 PRED_VERSION=patch-level-color.txt
 DIS_FOLDER=${OUT_DIR}/patch-level-color/
 for files in ${FOLDER}/*/${PRED_VERSION}; do
-    dis=`echo ${files} | awk -F'/' '{print "color-"substr($(NF-1),1,length($(NF-1))-4);}'`
+    fname=`echo ${files} | awk -F '/' '{print $(NF-1)}'`;
+    fprefix="${fname%.*}"
+    dis="color-"$fprefix
+    # dis=`echo ${files} | awk -F'/' '{print "color-"substr($(NF-1),1,length($(NF-1))-4);}'`
     cp ${files} ${DIS_FOLDER}/${dis}
 done
 
